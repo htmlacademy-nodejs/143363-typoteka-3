eaefa97940b0f8473bf5dddd80ab0ad0d7ac2ef1
Dmitry Kitsenko <kitsenko@greensight.ru>
2021-06-09 22:21:59 +0300

    move constants to txt files

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -3,14 +3,28 @@
 const chalk = require(`chalk`);
 const fs = require(`fs`).promises;
 const {getRandomInt, shuffle, getRandomItem, getRandomDate, getStartDate} = require(`../../utils`);
-const {MAX_COUNT, DEFAULT_COUNT, MAX_ANNOUNCE_LENGTH, FILE_NAME, PERIOD_MONTH, TITLES, CATEGORIES, ANNOUNCES, ExitCodes} = require(`../../constants`);
+const {MAX_COUNT, DEFAULT_COUNT, MAX_ANNOUNCE_LENGTH, FILE_NAME, PERIOD_MONTH, ExitCodes} = require(`../../constants`);
 
-const generateData = (count) => Array(count).fill({}).map(() => ({
-  title: getRandomItem(TITLES),
+const TITLES_FILE = `./data/titles.txt`;
+const SENTENCES_FILE = `./data/sentences.txt`;
+const CATEGORIES_FILE = `./data/categories.txt`;
+
+const readContent = async (fileName) => {
+  try {
+    const content = await fs.readFile(fileName, {encoding: `utf-8`});
+    return content.trim().split(`\n`);
+  } catch (e) {
+    console.error(chalk.red(`Не удалось прочитать файл ${fileName}: ${e}`));
+    return [];
+  }
+};
+
+const generateData = (count, titles, sentences, categories) => Array(count).fill({}).map(() => ({
+  title: getRandomItem(titles),
   createdDate: getRandomDate(getStartDate(PERIOD_MONTH), new Date()).toLocaleString(`ru`),
-  announce: shuffle(ANNOUNCES).slice(0, getRandomInt(1, MAX_ANNOUNCE_LENGTH)).join(` `),
-  fullText: shuffle(ANNOUNCES).slice(0, getRandomInt(1, ANNOUNCES.length)).join(` `),
-  сategory: shuffle(CATEGORIES).slice(0, getRandomInt(1, CATEGORIES.length)),
+  announce: shuffle(sentences).slice(0, getRandomInt(1, MAX_ANNOUNCE_LENGTH)).join(` `),
+  fullText: shuffle(sentences).slice(0, getRandomInt(1, sentences.length)).join(` `),
+  сategory: shuffle(categories).slice(0, getRandomInt(1, categories.length)),
 }));
 
 module.exports = {
@@ -23,7 +37,11 @@ module.exports = {
       process.exit(ExitCodes.ERROR);
     }
 
-    const content = JSON.stringify(generateData(publicationsCount));
+    const titles = await readContent(TITLES_FILE);
+    const sentences = await readContent(SENTENCES_FILE);
+    const categories = await readContent(CATEGORIES_FILE);
+
+    const content = JSON.stringify(generateData(publicationsCount, titles, sentences, categories));
 
     try {
       await fs.writeFile(FILE_NAME, content);
