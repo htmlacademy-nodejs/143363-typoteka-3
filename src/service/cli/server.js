6040d0124776959c1d99b1e65c4c76cbc81b66ba
Dmitry Kitsenko <kitsenko@greensight.ru>
2021-06-15 22:39:40 +0300

    server refactoring

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,45 +1,21 @@
 'use strict';
 
 const chalk = require(`chalk`);
-const http = require(`http`);
+const express = require(`express`);
 const fs = require(`fs`).promises;
-const {DEFAULT_PORT, HttpCode, FILE_NAME} = require(`../../constants`);
+const {DEFAULT_PORT, FILE_NAME} = require(`../../constants`);
 
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-    <!Doctype html>
-    <html lang="ru">
-      <head>
-        <title>Typoteka</title
-      </head>
-      <body>
-        ${message}
-      </body>
-    </html>`.trim();
 
-  res.writeHead(statusCode, {'Content-Type': `text/html; charset=UTF-8`});
-  res.end(template);
-};
-
-const onClientConnect = async (req, res) => {
-  const notFoundText = `Not found`;
-
-  switch (req.url) {
-    case `/`:
-      try {
-        const fileContent = await fs.readFile(FILE_NAME);
-        const mocks = JSON.parse(fileContent);
-        const message = `<ul>${mocks.map(({title}) => (`<li>${title}</li>`)).join(``)}</ul`;
-        sendResponse(res, HttpCode.OK, message);
-      } catch (e) {
-        sendResponse(res, HttpCode.NOT_FOUND, notFoundText);
-      }
-      break;
-    default:
-      sendResponse(res, HttpCode.NOT_FOUND, notFoundText);
-      break;
+const postsRouter = new express.Router();
+postsRouter.get(`/`, async (req, res) => {
+  try {
+    const fileContent = await fs.readFile(FILE_NAME);
+    const mocks = JSON.parse(fileContent);
+    res.json(mocks);
+  } catch (e) {
+    res.json([]);
   }
-};
+});
 
 module.exports = {
   name: `--server`,
@@ -47,11 +23,10 @@ module.exports = {
     const [customPort] = args;
     const port = parseInt(customPort, 10) || DEFAULT_PORT;
 
-    http.createServer(onClientConnect)
-      .listen(port)
-      .on(`listening`, () => console.info(chalk.green(`Server listening on port: ${port}`)))
-      .on(`error`, (err) => {
-        console.error(chalk.red(`Creation server error: ${err}`));
-      });
+    const app = express();
+    app.use(express.json());
+    app.use(`/posts`, postsRouter);
+
+    app.listen(port, () => console.info(chalk.green(`Server listening on port ${port}: http://localhost:${port}`)));
   }
 };
