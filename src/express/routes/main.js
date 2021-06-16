6d27586dd7ddd9bb411dd1bed8d90af7517a6bfe
Dmitry Kitsenko <kitsenko@greensight.ru>
2021-06-16 20:47:37 +0300

    templates and class fixes

--- src/express/routes/main.js
+++ src/express/routes/main.js
@@ -6,8 +6,8 @@ const mainRouter = new Router();
 
 mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
-mainRouter.get(`/categories`, (req, res) => res.render(`all-categories`));
-mainRouter.get(`/search`, (req, res) => res.render(`search`));
+mainRouter.get(`/categories`, (req, res) => res.render(`all-categories`, {noBg: true}));
+mainRouter.get(`/search`, (req, res) => res.render(`search`, {searchPage: true}));
 mainRouter.get(`/add`, (req, res) => res.render(`new-post`));
 mainRouter.get(`/`, (req, res) => res.render(`main`));
 
--- src/express/routes/my.js
+++ src/express/routes/my.js
@@ -4,7 +4,7 @@ const {Router} = require(`express`);
 
 const myRouter = new Router();
 
-myRouter.get(`/comments`, (req, res) => res.render(`comments`));
-myRouter.get(`/`, (req, res) => res.render(`my`, {admin: true}));
+myRouter.get(`/comments`, (req, res) => res.render(`comments`, {noBg: true}));
+myRouter.get(`/`, (req, res) => res.render(`my`, {admin: true, noBg: true}));
 
 module.exports = myRouter;
